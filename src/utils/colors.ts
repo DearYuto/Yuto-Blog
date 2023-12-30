2c32583a827cce7da69b13aedc15e8b202297b7f
DearYuto <reasons@kakao.com>
2023-12-31 01:17:35 +0900

    feat: 컬러 팔레트 모듈 파일 추가

---
+++ src/utils/colors.ts
@@ -0,0 +1,27 @@
+export const palette = {
+  // main
+  primary: '#7885EC',
+  primaryLight: '#AEB7FF',
+
+  // danger
+  danger: '#CB4C54',
+  dangerLight: '#F57E85',
+
+  // gray scale
+  white: '#FFFFFF',
+  gray50: '#F8F8F8',
+  gray100: '#F1F1F1',
+  gray150: '#EAEAEA',
+  gray200: '#E3E3E3',
+  gray300: '#C1C2CA',
+  gray400: '#A1A3AD',
+  gray500: '#7B7C85',
+  gray600: '#55565C',
+  gray700: '#3D3E41',
+  gray800: '#222325',
+
+  // etc.
+  serenity100: '#C8CDF1',
+  serenity50: '#E3E5F5',
+  grayPurple50: '#F4E3F5',
+};
