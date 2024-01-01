fb05f3241b2be1b4da91086bcc28f6f300e138c9
DearYuto <reasons@kakao.com>
2024-01-01 22:45:36 +0900

    feat(Text): 컴포넌트 docs 추가

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import type { Preview } from '@storybook/react';
+import {
+  Title,
+  Subtitle,
+  Description,
+  Primary,
+  Controls,
+  Stories,
+} from '@storybook/blocks';
 import '../src/styles/reset.css';
 
 const preview: Preview = {
@@ -10,6 +18,19 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    docs: {
+      // 커스텀 필요 시 수정하기 (현재는 디폴트 값으로 생략 가능)
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <Controls />
+          <Stories />
+        </>
+      ),
     },
   },
   decorators: [
--- src/components/atoms/text/Text.stories.tsx
+++ src/components/atoms/text/Text.stories.tsx
@@ -1,14 +1,21 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Text from './Text';
 
+/**
+ * 텍스트 컴포넌트입니다.
+ */
 const meta: Meta<typeof Text> = {
   component: Text,
+  tags: ['autodocs'],
 };
 
 export default meta;
 type Story = StoryObj<typeof Text>;
 
-export const DefaultText: Story = {
+/**
+ * 기본 텍스트 컴포넌트입니다.
+ */
+export const Default: Story = {
   args: {
     children: '유토의 하루',
     size: 'md',
@@ -17,11 +24,24 @@ export const DefaultText: Story = {
   },
 };
 
-export const PrimaryColorText: Story = {
+/**
+ * 메인 컬러 + 텍스트 강조 시 사용됩니다.
+ */
+export const PrimaryColor: Story = {
   args: {
     children: '유토의 하루',
-    size: 'lg',
+    size: 'md',
     color: 'primary',
     weight: 'bold',
+  },
+};
+
+export const Big: Story = {
+  args: {
+    children: '유토의 하루',
+    size: '2xl',
+    color: 'gray700',
+    weight: 'bold',
+    as: 'h1',
   },
 };
