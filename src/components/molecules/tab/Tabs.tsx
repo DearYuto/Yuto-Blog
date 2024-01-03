4a18c7490b1475b3b0f50e6819874bfa49731c14
DearYuto <reasons@kakao.com>
2024-01-04 00:21:33 +0900

    feat(Tabs): 탭스 컴포넌트 예외처리 로직 추가

--- src/components/molecules/tab/Tabs.tsx
+++ src/components/molecules/tab/Tabs.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import Tab from './Tab';
+'use client';
+
+import React, { useState } from 'react';
+import Tab, { TabProps } from './Tab';
 import Flex from '@/components/atoms/flex/Flex';
 
 type TabsProps = {
-  children: React.ReactNode;
+  activeTabIndex?: number;
+  children: React.ReactElement<TabProps>[];
 };
 
-function TabContainer({ children }: TabsProps) {
+function TabContainer({ children, activeTabIndex = 0 }: TabsProps) {
+  checkActiveIndex({ children, activeTabIndex });
+
+  const [activeTab, setActiveTab] = useState(0);
+
   return <Flex>{children}</Flex>;
 }
 
 const Tabs = Object.assign(TabContainer, { Tab });
 export default Tabs;
+
+/**
+ * 예외처리 코드
+ */
+const checkActiveIndex = ({ activeTabIndex, children }: TabsProps) => {
+  if (!activeTabIndex) return;
+
+  if (activeTabIndex < 0 || activeTabIndex > children.length) {
+    throw new Error(
+      `activeTabIndex는 0 ~ ${children.length - 1} 범위 값으로 지정해야 합니다.`
+    );
+  }
+};
