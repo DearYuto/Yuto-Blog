84a3645afa563df90c1e0be8ea89b8f3c7b3a38c
DearYuto <reasons@kakao.com>
2024-01-03 23:51:10 +0900

    feat(Tab): onClick 프롭스 추가

--- src/components/molecules/tab/Tab.tsx
+++ src/components/molecules/tab/Tab.tsx
@@ -1,15 +1,25 @@
 import Text from '@/components/atoms/text/Text';
 import { tab } from './tab.css';
 
-type TabProps = {
+export type TabProps = {
   label: string;
   children: React.ReactNode;
   active?: boolean;
+  onClick: () => void;
 };
-export default function Tab({ label, children, active = false }: TabProps) {
+export default function Tab({
+  label,
+  children,
+  onClick,
+  active = false,
+}: TabProps) {
   return (
     <>
-      <div role='tab' className={`${tab[active ? 'active' : 'inactive']}`}>
+      <div
+        onClick={onClick}
+        role='tab'
+        className={`${tab[active ? 'active' : 'inactive']}`}
+      >
         <Text color={active ? 'primary' : 'gray700'}>{label}</Text>
       </div>
     </>
