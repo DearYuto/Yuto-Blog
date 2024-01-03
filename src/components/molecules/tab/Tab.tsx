7cdb0fb2e849e49ea14cb2016bbb7bbfe1cc0151
DearYuto <reasons@kakao.com>
2024-01-04 02:19:28 +0900

    feat(Tab): onClick 이벤트 추가, label 제거 후 children으로 변경

--- src/components/molecules/tab/Tab.tsx
+++ src/components/molecules/tab/Tab.tsx
@@ -1,27 +1,21 @@
+'use client';
+
 import Text from '@/components/atoms/text/Text';
 import { tab } from './tab.css';
 
 export type TabProps = {
-  label: string;
   children: React.ReactNode;
   active?: boolean;
-  onClick: () => void;
+  onClick?: () => void;
 };
-export default function Tab({
-  label,
-  children,
-  onClick,
-  active = false,
-}: TabProps) {
+export default function Tab({ onClick, children, active = false }: TabProps) {
   return (
-    <>
-      <div
-        onClick={onClick}
-        role='tab'
-        className={`${tab[active ? 'active' : 'inactive']}`}
-      >
-        <Text color={active ? 'primary' : 'gray700'}>{label}</Text>
-      </div>
-    </>
+    <div
+      onClick={onClick}
+      role='tab'
+      className={`${tab[active ? 'active' : 'inactive']}`}
+    >
+      <Text color={active ? 'primary' : 'gray700'}>{children}</Text>
+    </div>
   );
 }
