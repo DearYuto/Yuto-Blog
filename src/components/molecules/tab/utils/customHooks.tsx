f3f44d7aaf3bb166a134be95260e28e26c88aab8
DearYuto <reasons@kakao.com>
2024-01-12 11:11:25 +0900

    chore: 코드 위치 수정

--- src/components/molecules/tab/utils/customHooks.tsx
+++ src/components/molecules/tab/utils/customHooks.tsx
@@ -1,18 +1,17 @@
 import { Dispatch, SetStateAction, createContext, useContext } from 'react';
 
-export const TabsStateContext = createContext<number>(0);
-export const TabsDispatchContext = createContext<
-  Dispatch<SetStateAction<number>>
->(() => 0);
-
 /**
  * context 사용
  * use 커스텀 훅
  */
+const TabsStateContext = createContext<number>(0);
 export function useActiveTab() {
   return useContext(TabsStateContext);
 }
 
+const TabsDispatchContext = createContext<Dispatch<SetStateAction<number>>>(
+  () => 0
+);
 export function useActiveTabDispatch() {
   return useContext(TabsDispatchContext);
 }
