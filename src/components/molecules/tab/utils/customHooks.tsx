65e0c9de510158d384e9d713502e86c236e61131
DearYuto <reasons@kakao.com>
2024-01-12 11:13:12 +0900

    feat: export 키워드 추가

--- src/components/molecules/tab/utils/customHooks.tsx
+++ src/components/molecules/tab/utils/customHooks.tsx
@@ -4,14 +4,14 @@ import { Dispatch, SetStateAction, createContext, useContext } from 'react';
  * context 사용
  * use 커스텀 훅
  */
-const TabsStateContext = createContext<number>(0);
+export const TabsStateContext = createContext<number>(0);
 export function useActiveTab() {
   return useContext(TabsStateContext);
 }
 
-const TabsDispatchContext = createContext<Dispatch<SetStateAction<number>>>(
-  () => 0
-);
+export const TabsDispatchContext = createContext<
+  Dispatch<SetStateAction<number>>
+>(() => 0);
 export function useActiveTabDispatch() {
   return useContext(TabsDispatchContext);
 }
