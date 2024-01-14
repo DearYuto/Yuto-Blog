59eb7881ff2c7216cfdc198c286664969ff72504
DearYuto <reasons@kakao.com>
2024-01-14 19:44:28 +0900

    fix(checkValidation): index 범위를 넘어가는 문제 수정

--- src/components/molecules/tab/utils/checkValidation.ts
+++ src/components/molecules/tab/utils/checkValidation.ts
@@ -3,7 +3,7 @@ import { TabsProps } from '../Tabs';
 export const checkActiveIndex = ({ activeTabIndex, children }: TabsProps) => {
   if (!activeTabIndex) return;
 
-  if (activeTabIndex < 0 || activeTabIndex > children.length) {
+  if (activeTabIndex < 0 || activeTabIndex > children.length - 1) {
     throw new Error(
       `activeTabIndex는 0 ~ ${children.length - 1} 범위 값으로 지정해야 합니다.`
     );
