f50f555aa2c3aa9835e92de045fff30a4d62d53c
DearYuto <reasons@kakao.com>
2024-01-20 21:55:57 +0900

    feat(MarkdownViewer): 마크다운 컴포넌트 태그 추가

--- src/components/organisms/markdown/MarkdownViewer.tsx
+++ src/components/organisms/markdown/MarkdownViewer.tsx
@@ -2,7 +2,8 @@ import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { container } from './markdown.css';
+import { blockquote, code, container, hr } from './markdown.css';
+import Text from '@/components/atoms/text/Text';
 
 type Props = {
   children: string;
@@ -15,10 +16,52 @@ export default function MarkdownViewer({ children }: Props) {
         className={container}
         remarkPlugins={[remarkGfm]}
         components={{
+          h1({ children }) {
+            return (
+              <Text
+                style={{ margin: '10px 0' }}
+                weight='bold'
+                size='2xl'
+                color='primary'
+                as='h1'
+              >
+                {children}
+              </Text>
+            );
+          },
+
+          h2({ children }) {
+            return (
+              <Text
+                style={{ margin: '10px 0' }}
+                weight='bold'
+                size='xl'
+                color='gray700'
+                as='h2'
+              >
+                {children}
+              </Text>
+            );
+          },
+
+          h3({ children }) {
+            return (
+              <Text
+                style={{ margin: '10px 0' }}
+                weight='bold'
+                size='xl'
+                color='gray700'
+                as='h3'
+              >
+                {children}
+              </Text>
+            );
+          },
+
           code({ className, children, ...props }) {
             const match = /language-(\w+)/.exec(className || '')!;
             return match ? (
-              <div style={{ color: '#fff' }}>
+              <div>
                 <SyntaxHighlighter
                   PreTag='div'
                   style={atomDark}
@@ -28,8 +71,20 @@ export default function MarkdownViewer({ children }: Props) {
                 </SyntaxHighlighter>
               </div>
             ) : (
-              <code {...props}>{children}</code>
+              <code className={code}>
+                <Text as='span' color='primary' size='sm' weight='bold'>
+                  {children}
+                </Text>
+              </code>
             );
+          },
+
+          blockquote({ children }) {
+            return <blockquote className={blockquote}>{children}</blockquote>;
+          },
+
+          hr() {
+            return <hr className={hr} />;
           },
         }}
       >
