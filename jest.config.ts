6b265f6c8ab9cb0c8d38178e7c4d45239a989689
DearYuto <reasons@kakao.com>
2024-01-23 18:06:54 +0900

    feat: jest 환경 세팅이 잘못되어 테스트 코드가 제대로 실행되지 않는 문제가 있어 config, setup 파일 수정 및 추가

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,13 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
+import nextJest from 'next/jest';
 
-/** @type {import('jest').Config} */
-const config = {
-  coverageProvider: 'v8',
+// Providing the path to your Next.js app which will enable loading next.config.js and .env files
+const createJestConfig = nextJest({ dir: './' });
+
+// Any custom config you want to pass to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jsdom',
 };
 
-module.exports = config;
+// createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
+export default createJestConfig(customJestConfig);
