44b591785ddc562e4bac66bf55c64abec762b36e
DearYuto <reasons@kakao.com>
2024-02-17 18:38:28 +0900

    fix(Modal): 모달 내부 컴포넌트(children)가 배열로 들어오지 않는 경우 발생하는 에러 처리

--- src/components/molecules/modal/Modal.tsx
+++ src/components/molecules/modal/Modal.tsx
@@ -2,22 +2,43 @@ import React from 'react';
 import Button from '@/components/atoms/button/Button';
 import ModalContents from './components/ModalContents';
 import ModalOverlay from './components/ModalOverlay';
+import ModalCloseButton from './components/ModalCloseButton';
 
 type Props = {
   isOpen: boolean;
-  children: React.ReactElement<typeof ModalContents>;
+  children: React.ReactElement | React.ReactElement[];
   openModal: () => void;
   closeModal: () => void;
 };
 
 function ModalContainer({ openModal, closeModal, children, isOpen }: Props) {
   if (children) {
-    const { displayName } = children.type as unknown as { displayName: string };
+    const allowedDisplayNames = ['Modal.Contents', 'Modal.CloseButton'];
 
-    if (displayName !== 'Modal.Contents') {
-      throw new Error(
-        '모달의 자식 요소는 반드시 ModalContents 컴포넌트를 사용해야 합니다.'
-      );
+    if ('length' in children) {
+      children.map((child) => {
+        const { displayName } = child.type as unknown as {
+          displayName: string;
+        };
+
+        if (!allowedDisplayNames.includes(displayName)) {
+          throw new Error(
+            `모달의 자식 요소는 반드시 ${allowedDisplayNames.join(
+              ' 또는 '
+            )} 컴포넌트를 사용해야 합니다.`
+          );
+        }
+      });
+    } else {
+      const { displayName } = children.type as unknown as {
+        displayName: string;
+      };
+
+      if (displayName !== 'Modal.Contents') {
+        throw new Error(
+          '모달의 자식 요소는 반드시 ModalContents 컴포넌트를 사용해야 합니다.'
+        );
+      }
     }
   }
 
@@ -38,6 +59,7 @@ ModalContainer.displayName = 'Modal';
 
 const Modal = Object.assign(ModalContainer, {
   Contents: ModalContents,
+  CloseButton: ModalCloseButton,
 });
 
 export default Modal;
